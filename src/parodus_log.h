869ed9e5fd594ae512467892a27a74738e83a5f0
Shilpa Seshadri <shilpa_seshadri@cable.comcast.com>
2018-07-29 23:35:34 -0700

    To print debug logs changing level to info

--- src/parodus_log.h
+++ src/parodus_log.h
@@ -32,4 +32,4 @@
 
 #define ParodusError(...)                   cimplog_error(LOGGING_MODULE, __VA_ARGS__)
 #define ParodusInfo(...)                    cimplog_info(LOGGING_MODULE, __VA_ARGS__)
-#define ParodusPrint(...)                   cimplog_debug(LOGGING_MODULE, __VA_ARGS__)
+#define ParodusPrint(...)                   cimplog_info(LOGGING_MODULE, __VA_ARGS__)
